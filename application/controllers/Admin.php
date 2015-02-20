3f6a25066cc5af735c3bc7a4d020929b95e680a6
Calvin Rempel <rempel.calvin@gmail.com>
2015-02-19 21:35:58 -0800

    -Added quote creation confirmation/error checking. -Added error message output to form presentation.
    Signed-off-by: Calvin Rempel <rempel.calvin@gmail.com>

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -33,9 +33,60 @@ class Admin extends Application {
         $this->present($quote);
     }
     
+    function confirm()
+    {
+        $record = $this->quotes->create();
+        
+        // Get Input
+        $record->id = $this->input->post('id');
+        $record->who = $this->input->post('who');
+        $record->mug = $this->input->post('mug');
+        $record->what = $this->input->post('what');
+        
+        // Error Check
+        if (empty($record->who))
+            $this->errors[] = 'You must specify an author.';
+        if (strlen($record->what) < 20)
+            $this->errors[] = 'A quotation must be at least 20 characters long.';
+        
+        // Redisplay if errors were found
+        if (count($this->errors) > 0)
+        {
+            $this->present($record);
+        }
+        else
+        {
+            // Save the Quote
+            if (empty($record->id))
+                $this->quotes->add($record);
+            else
+                $this->quotes->update($record);
+
+            redirect('/admin');
+        }
+    }
+    
     function present($quote)
     {
-        $this->data['fid'] = makeTextField('ID#', 'id', $quote->id);
+        // Create Error Message Output
+        $message = '';
+        if (count($this->errors) > 0)
+        {
+            foreach($this->errors as $error)
+            {
+                $message .= $error . BR;
+            }
+        }
+        $this->data['message'] = $message;
+        
+        // Create Form
+        $this->data['fid'] = makeTextField('ID#',
+                                           'id',
+                                           $quote->id,
+                                           "Unique quote identifier,system-assigned",
+                                           10,
+                                           10,
+                                           true);
         $this->data['fwho'] = makeTextField('Author', 'who', $quote->who);
         $this->data['fmug'] = makeTextField('Picture', 'mug', $quote->mug);
         $this->data['fwhat'] = makeTextArea('The Quote', 'what', $quote->what);
